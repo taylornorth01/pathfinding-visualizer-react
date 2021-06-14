59cde3cea07ca715b837e85d3346a40513265406
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-14 23:25:52 +0100

    place flag function

--- src/common/GridUtility.ts
+++ src/common/GridUtility.ts
@@ -1,9 +1,8 @@
 //
-import { GridObject, GridNode } from '../model';
+import { GridObject, GridNode, FlagType, Position } from '../model';
 
 export const createGrid = (h: number, w: number): GridObject => {
 	let grid: GridNode[][] = [];
-
 	for (let y = 0; y < h; y++) {
 		let row: GridNode[] = [];
 		for (let x = 0; x < w; x++) {
@@ -11,6 +10,14 @@ export const createGrid = (h: number, w: number): GridObject => {
 		}
 		grid.push(row);
 	}
+	return grid;
+};
 
+export const placeFlag = (
+	flag: FlagType,
+	pos: Position,
+	grid: GridObject
+): GridObject => {
+	grid[pos.y][pos.x].type = flag;
 	return grid;
 };
