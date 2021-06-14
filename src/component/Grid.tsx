8ff49ff934d20bcbaf8a3a5c99eb33d69802ef1f
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-14 23:48:13 +0100

    mouse enter event returns node

--- src/component/Grid.tsx
+++ src/component/Grid.tsx
@@ -6,9 +6,10 @@ import '../style/grid.css';
 interface Props {
 	grid: GridObject;
 	mouseDown: (node: GridNode) => void;
+	mouseEnter: (node: GridNode) => void;
 }
 
-const Grid: React.FC<Props> = ({ grid, mouseDown }) => {
+const Grid: React.FC<Props> = ({ grid, mouseDown, mouseEnter }) => {
 	return (
 		<div>
 			{grid.map((row) => {
@@ -19,7 +20,8 @@ const Grid: React.FC<Props> = ({ grid, mouseDown }) => {
 								return (
 									<div
 										className='grid__node'
-										onMouseDown={() => mouseDown(node)}>
+										onMouseDown={() => mouseDown(node)}
+										onMouseEnter={() => mouseEnter(node)}>
 										{node.type}
 									</div>
 								);
