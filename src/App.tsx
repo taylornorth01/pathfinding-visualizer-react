c730883d71f92f0076f131c1d9023bfa19261c01
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-14 23:26:15 +0100

    state start and goal position init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 //
 import { useReducer } from 'react';
 import './app.css';
-import { createGrid } from './common/';
+import { createGrid, placeFlag } from './common/';
 import Grid from './component/Grid';
-import { GridObject } from './model/';
+import { GridObject, Position } from './model/';
 
 interface State {
 	grid: GridObject;
+	start: Position;
+	goal: Position;
 }
 
 type Actions = { type: 'example' };
+
+const init = (initial: State) => {
+	initial.grid = placeFlag('start', initial.start, initial.grid);
+	initial.grid = placeFlag('goal', initial.goal, initial.grid);
+	return initial;
+};
 
 const reducer = (state: State, action: Actions) => {
 	switch (action.type) {
@@ -21,7 +29,15 @@ const reducer = (state: State, action: Actions) => {
 };
 
 const App: React.FC = () => {
-	const [state, dispatch] = useReducer(reducer, { grid: createGrid(5, 5) });
+	const [state, dispatch] = useReducer(
+		reducer,
+		{
+			grid: createGrid(5, 5),
+			start: { x: 0, y: 0 },
+			goal: { x: 4, y: 4 },
+		},
+		init
+	);
 
 	return (
 		<div className='app'>
