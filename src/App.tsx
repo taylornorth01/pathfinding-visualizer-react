c1e7b4f6cdf2115459a4d2f85278244c579fce6e
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-14 22:51:44 +0100

    basic app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//
+import { useReducer } from 'react';
+import './app.css';
+import { createGrid } from './common/';
+import { Grid } from './model/';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+	grid?: Grid;
 }
+
+type Actions = { type: 'example' };
+
+const reducer = (state: State, action: Actions) => {
+	switch (action.type) {
+		case 'example':
+			return state;
+		default:
+			throw new Error('Reducer action not found');
+	}
+};
+
+const App: React.FC = () => {
+	const [state, dispatch] = useReducer(reducer, { grid: createGrid(5, 5) });
+
+	return <div className='app'>{JSON.stringify(state.grid)}</div>;
+};
 
 export default App;
