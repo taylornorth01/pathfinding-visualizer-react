cec838c41ddebbf0f5fcd332e8e7aba4e9b8ec35
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-15 20:17:41 +0100

    block type for wall and empty

--- src/model/GridModel.ts
+++ src/model/GridModel.ts
@@ -1,7 +1,8 @@
 //
 export type FlagType = 'start' | 'goal';
+export type BlockType = 'empty' | 'wall';
 export type Visited = 'open' | 'closed' | 'current';
-export type NodeType = FlagType | 'empty' | 'wall';
+export type NodeType = FlagType | BlockType;
 
 export interface Position {
 	x: number;
@@ -12,6 +13,7 @@ export interface GridNode {
 	pos: Position;
 	state?: Visited;
 	type: NodeType;
+	overided?: BlockType;
 }
 
 export type GridObject = GridNode[][];
