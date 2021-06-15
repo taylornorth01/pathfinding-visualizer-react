56dea0fc438a28f553fef6d763c6e173dd48e3a5
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-15 23:24:33 +0100

    get algorithm class function

--- src/common/AlgorithmUtility.ts
+++ src/common/AlgorithmUtility.ts
@@ -1,5 +1,6 @@
 //
-import { AlgorithmId, AlgorithmObject } from '../model';
+import { Astar } from '../algorithm';
+import { AlgorithmId, AlgorithmObject, GridObject, Position } from '../model';
 
 export const getAlgorithmObject = ({ id }: AlgorithmId): AlgorithmObject => {
 	switch (id) {
@@ -9,6 +10,20 @@ export const getAlgorithmObject = ({ id }: AlgorithmId): AlgorithmObject => {
 			return { id: 'dijkstra', name: 'Dijkstra' };
 		case 'depthfs':
 			return { id: 'depthfs', name: 'Depth First Search' };
+		default:
+			throw new Error('Algorithm not found');
+	}
+};
+
+export const getAlgorithmClass = (
+	{ id }: AlgorithmId,
+	grid: GridObject,
+	start: Position,
+	goal: Position
+): Astar => {
+	switch (id) {
+		case 'astar':
+			return new Astar(grid, start, goal);
 		default:
 			throw new Error('Algorithm not found');
 	}
