1c7cde9e83094267098d24f95582e069e6ebeb32
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-16 13:34:48 +0100

    update algorithm function

--- src/common/AlgorithmUtility.ts
+++ src/common/AlgorithmUtility.ts
@@ -1,6 +1,7 @@
 //
 import { Astar } from '../algorithm';
 import { AlgorithmId, AlgorithmObject, AppState } from '../model';
+import { clearPath, mapNodesStateToGrid } from './GridUtility';
 
 export const getAlgorithmObject = (
 	{ grid, start, goal }: AppState,
@@ -13,11 +14,24 @@ export const getAlgorithmObject = (
 				name: 'A*',
 				get: () => new Astar(grid, start, goal),
 			};
-		case 'dijkstra':
-			return { id: 'dijkstra', name: 'Dijkstra', get: () => null };
-		case 'depthfs':
-			return { id: 'depthfs', name: 'Depth First Search', get: () => null };
+		// case 'dijkstra':
+		// 	return { id: 'dijkstra', name: 'Dijkstra', get: () => null };
+		// case 'depthfs':
+		// 	return { id: 'depthfs', name: 'Depth First Search', get: () => null };
 		default:
 			throw new Error('Algorithm not found');
 	}
+};
+
+export const updateAlgorithm = (state: AppState): AppState => {
+	let algorithm,
+		grid = state.grid;
+	if (state.algorithm) {
+		algorithm = getAlgorithmObject(state, state.algorithm);
+		if (state.wasSearch) {
+			grid = clearPath(grid);
+			grid = mapNodesStateToGrid(algorithm.get().result().squares, grid);
+		}
+	}
+	return { ...state, algorithm, grid };
 };
