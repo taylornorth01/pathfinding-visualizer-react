f653f1094fa3be3a93d110b7311238852d967681
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-16 15:20:08 +0100

    random maze algorithm

---
+++ src/algorithm/RandomMaze.ts
@@ -0,0 +1,24 @@
+//
+
+import { GridObject } from '../model';
+
+export class RandomMaze {
+	grid: GridObject;
+
+	constructor(grid: GridObject) {
+		this.grid = grid;
+	}
+
+	result = () => {
+		return this.grid.map((row) => {
+			return row.map((node) => {
+				if (node.type === 'start' || node.type === 'goal') return node;
+				if (Math.random() > 0.7) {
+					node.type = 'wall';
+					return node;
+				}
+				return node;
+			});
+		});
+	};
+}
