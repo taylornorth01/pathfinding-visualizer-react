09c77c8f7e838e269ef4f6c52b72a48dd9111816
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-20 11:00:12 +0100

    list icon

---
+++ src/icon/ListIcon.tsx
@@ -0,0 +1,21 @@
+//
+interface Props {
+	size: number;
+}
+
+export const ListIcon: React.FC<Props> = ({ size }) => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width={size}
+			height={size}
+			fill='currentColor'
+			className='bi bi-list'
+			viewBox='0 0 16 16'>
+			<path
+				fill-rule='evenodd'
+				d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
+			/>
+		</svg>
+	);
+};
