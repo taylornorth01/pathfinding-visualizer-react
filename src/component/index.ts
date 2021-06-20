afc8c29706297347a3a7c5dd14d3982dc9ad2771
taylornorth01 <taylornorth01@users.noreply.github.com>
2021-06-20 11:35:20 +0100

    new exports

--- src/component/index.ts
+++ src/component/index.ts
@@ -3,3 +3,7 @@ export * from './Grid';
 export * from './Dropdown';
 export * from './Option';
 export * from './Button';
+export * from './List';
+export * from './Column';
+export * from './Item';
+export * from './Radio';
